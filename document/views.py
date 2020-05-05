1f3abbefd3b80dfee6c62fb845eac7a6c4163abb
lelyavin_907 <nlelyavin1@yandex.ru>
2020-05-05 23:27:31 +0300

    s

--- document/views.py
+++ document/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import UpdateView
 from django.urls import reverse_lazy
 from document.models import SiteUser
+from docxtpl import DocxTemplate
+from random import randint
 
 
 def index(request):
@@ -23,6 +25,20 @@ def info(request):
         return render(request, 'info_123.html', context={'site_user': site_user})
     else:
         return render(request, 'info_123.html')
+
+
+def document(request):
+    site_user = SiteUser.objects.get(user=request.user)
+    name = site_user.passport.series
+    passport = site_user.passport.series
+    doc = DocxTemplate("document/test.docx")
+    context = {'name': name, 'nomer': passport}
+    doc.render(context)
+    # document = Document('test.docx')
+    # document.save('example.docx')
+    id = randint(1, 10000000)
+    doc.save("document/documents/" + str(id) +".docx")
+    return render(request, 'index.html', context={'site_user': site_user})
 
 
 def statements(request):
