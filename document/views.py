32c81084ac67308795f4e0a567f51d54a1f1d4fb
lelyavin_907 <nlelyavin1@yandex.ru>
2020-05-20 03:34:28 +0300

    S

--- document/views.py
+++ document/views.py
@@ -35,75 +35,50 @@ def login(request):
  #   if request.method == "POST":
 
 
-
 def document(request):
     if request.method == "POST":
-        if request.user.is_authenticated:
-            id = request.user.last_name
-            doc = DocxTemplate("document/docExample/socPitanie.docx")
-            course = request.POST['course']
-            group = request.POST['group']
-            context = {"group": group, "course": course}
-            doc.render(context)
-            doc.save("document/documents/" + str(id) + ".docx")
-
-            excel_file_name = "document/documents/" + str(id) + ".docx"
-            fp = open(excel_file_name, "rb")
-            response = HttpResponse(fp.read())
-            fp.close()
-
-            file_type = mimetypes.guess_type(excel_file_name)
-            if file_type is None:
-                file_type = 'application/octet-stream'
-            response['Content-Type'] = file_type
-            response['Content-Length'] = str(os.stat(excel_file_name).st_size)
-            response['Content-Disposition'] = "attachment; filename= " + str(id) + ".docx"
-            os.remove(excel_file_name)
-            return response
-        else:
-            doc = DocxTemplate("document/docExample/socPitanie.docx")
-            course = request.POST['course']
-            group = request.POST['group']
-            nameHeadman = request.POST['nameHeadman']
-            name_institute = request.POST['name_institute']
-            series = request.POST['series']
-            number = request.POST['number']
-            code = request.POST['code']
-            dateTimeField = request.POST['dateTimeField']
-            INN = request.POST['INN']
-            place = request.POST['place']
-            dateBirthday = request.POST['dateBirthday']
-            numberPhone = request.POST['phoneNumber']
-            certificate = request.POST['numberInsuranceCertificate']
-            dateBirthday = request.POST['dateBirthday']
-            pFact = request.POST['pFact']
-            invalid = request.POST['disability_group']
-            invalid2 = request.POST['disability_group_text']
-            answer = request.POST['fullStateSupport']
-            surname = request.POST['surname']
-            name = request.POST['name']
-            patronymic = request.POST['patronymic']
-
-            context = {"group": group, "course": course, "starosta": nameHeadman, "name": name_institute,
+        doc = DocxTemplate("document/docExample/socPitanie.docx")
+        course = request.POST['course']
+        group = request.POST['group']
+        nameHeadman = request.POST['nameHeadman']
+        name_institute = request.POST['name_institute']
+        series = request.POST['series']
+        number = request.POST['number']
+        code = request.POST['code']
+        dateTimeField = request.POST['dateTimeField']
+        INN = request.POST['INN']
+        place = request.POST['place']
+        dateBirthday = request.POST['dateBirthday']
+        numberPhone = request.POST['phoneNumber']
+        certificate = request.POST['numberInsuranceCertificate']
+        dateBirthday = request.POST['dateBirthday']
+        pFact = request.POST['pFact']
+        invalid = request.POST['disability_group']
+        invalid2 = request.POST['disability_group_text']
+        answer = request.POST['fullStateSupport']
+        surname = request.POST['surname']
+        name = request.POST['name']
+        patronymic = request.POST['patronymic']
+        context = {"group": group, "course": course, "starosta": nameHeadman, "name": name_institute,
                        "nomer": number, "series": series,  "vidan": place, "inn": INN, "adress": pFact,
                         "svidetel": certificate, "dateNumber": dateBirthday, "invalid": invalid, "invalid2": invalid2,
                        "answer": answer, "numberPhone": numberPhone, "sur": surname, "nam": name, "otchet": patronymic}
-            doc.render(context)
-            doc.save("document/documents/anonym.docx")
+        doc.render(context)
+        doc.save("document/documents/anonym.docx")
 
-            excel_file_name = "document/documents/anonym.docx"
-            fp = open(excel_file_name, "rb")
-            response = HttpResponse(fp.read())
-            fp.close()
+        excel_file_name = "document/documents/anonym.docx"
+        fp = open(excel_file_name, "rb")
+        response = HttpResponse(fp.read())
+        fp.close()
 
-            file_type = mimetypes.guess_type(excel_file_name)
-            if file_type is None:
-                file_type = 'application/octet-stream'
-            response['Content-Type'] = file_type
-            response['Content-Length'] = str(os.stat(excel_file_name).st_size)
-            response['Content-Disposition'] = "attachment; filename= anonym.docx"
-            os.remove(excel_file_name)
-            return response
+        file_type = mimetypes.guess_type(excel_file_name)
+        if file_type is None:
+            file_type = 'application/octet-stream'
+        response['Content-Type'] = file_type
+        response['Content-Length'] = str(os.stat(excel_file_name).st_size)
+        response['Content-Disposition'] = "attachment; filename= anonym.docx"
+        os.remove(excel_file_name)
+        return response
 
 
 def statements(request):
